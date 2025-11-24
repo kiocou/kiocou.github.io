fa4c9716b6e9ae167addf5a040eecad940bd47b4
zhoshusheng <zhoshusheng@foxmail.com>
2025-11-24 22:55:33 +0800

    Update siteURL to GitHub Pages URL

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "Kiocou",
 	subtitle: "Hellow   Kiocou",
-	siteURL: "https://zhoss.eu.org/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://kiocou.github.io/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-11-24", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
